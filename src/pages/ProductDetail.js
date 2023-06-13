8d3ac6b180bf6b00857cb6e4624d67b4fb00c1df
OwenSyKim <owen.sykim@gmail.com>
2023-06-13 13:29:50 -0700

    setup json server

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -11,7 +11,7 @@ const ProductDetail = () => {
 
     const getProductDetail = async () => {
         setLoading(true);
-        let url = `http://localhost:5000/products/${id}`
+        let url = `https://my-json-server.typicode.com/legowen/hnm-react-router/products/${id}`
         let response = await fetch(url);
         let data = await response.json();
         setLoading(false);
