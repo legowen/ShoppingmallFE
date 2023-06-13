9033665f053985fe6e09178414f085be10ae51f3
OwenSyKim <owen.sykim@gmail.com>
2023-06-13 12:55:09 -0700

    re-edited url

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -11,7 +11,7 @@ const ProductDetail = () => {
 
     const getProductDetail = async () => {
         setLoading(true);
-        let url = `https://my-json-server.typicode.com/legowen/hnm-react-router/products?q=${id}`
+        let url = `https://my-json-server.typicode.com/legowen/hnm-react-router/products/${id}`
         let response = await fetch(url);
         let data = await response.json();
         setLoading(false);
