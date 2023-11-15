bbcd1b5aad488e835af0e772865cb916fcb442df
OwenSyKim <owen.sykim@gmail.com>
2023-11-15 15:32:12 -0800

    Setup&Enhanced FE for BE

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProductAll from "./pages/ProductAll";
-import Login from "./pages/Login";
-import PrivateRoute from "./routes/PrivateRoute";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "./action/userAction";
+import "./style/common.style.css";
+import AppLayout from "./Layout/AppLayout";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  let [authenticate, setAuthenticate] = useState(false); //If it is true signin, if it is false, stay signout.
-  useEffect(() => {
-    console.log("AAA", authenticate);
-  }, [authenticate]);
-
   return (
     <div>
-      <Navbar authenticate={authenticate} setAuthenticate={setAuthenticate} />
-      <Routes>
-        <Route path="/" element={<ProductAll />} />
-
-        <Route
-          path="/login"
-          element={<Login setAuthenticate={setAuthenticate} />}
-        />
-
-        <Route
-          path="/product/:id"
-          element={<PrivateRoute authenticate={authenticate} />}
-        />
-      </Routes>
+      <AppLayout>
+        <AppRouter />
+      </AppLayout>
     </div>
-    // /product/:id => Restful Route's Rule
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./reducer/store";
+// const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
