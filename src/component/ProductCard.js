22c8fbb1d79c91450b37cb23e9175da697475fb6
OwenSyKim <owen.sykim@gmail.com>
2023-12-14 16:31:32 -0800

    Recoded Pagenation, ProductCard

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -2,19 +2,17 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { currencyFormat } from "../utils/number";
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
   const navigate = useNavigate();
   const showProduct = (id) => {
     // Move to Product Item detail page  //  상품 디테일 페이지로 가기
+    navigate(`/product/${id}`);
   };
   return (
-    <div className="card" onClick={() => showProduct("hard_code")}>
-      <img
-        src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F3a%2F04%2F3a04ededbfa6a7b535e0ffa30474853fc95d2e81.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]"
-        alt=""
-      />
-      <div>Linen-blend Pull-on Shirt</div>
-      <div>$45</div>
+    <div className="card" onClick={() => showProduct(item._id)}>
+      <img src={item?.image} alt={item?.image} />
+      <div>{item?.name}</div>
+      <div>₩ {currencyFormat(item?.price)}</div>
     </div>
   );
 };
