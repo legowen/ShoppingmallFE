f991d8ca82d5e2919db8829e07704f92577b2b55
OwenSyKim <owen.sykim@gmail.com>
2024-01-10 15:24:58 -0800

    Add Google Login Token

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -45,7 +45,11 @@ const loginWithGoogle = (token) => async (dispatch) => {
     dispatch({ type: types.GOOGLE_LOGIN_REQUEST });
     const response = await api.post("/auth/google", { token });
     if (response.status !== 200) throw new Error(response.error);
-    dispatch({ type: types.GOOGLE_LOGIN_SUCCESS, payload: response.data });
+    sessionStorage.setItem("token", response.data.token);
+    dispatch({
+      type: types.GOOGLE_LOGIN_SUCCESS,
+      payload: response.data,
+    });
   } catch (error) {
     dispatch({ type: types.GOOGLE_LOGIN_FAIL, payload: error.error });
     dispatch(commonUiActions.showToastMessage(error.error, "error"));
